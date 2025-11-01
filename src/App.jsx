880e3df93fafe2c01b295e6cfa6ce53e2f06cf6b
sugonhwang <sugonhwang1@gmail.com>
2025-11-01 22:54:17 +0900

    💄 카운터 증감시 애니메이션 동작

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import CounterBox from "./components/CounterBox";
 import counterStore from "./stores/CounterStore";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   const { count, increase, decrease, increaseBy } = counterStore();
+  const [animate, setAnimate] = useState(false);
+
+  // 숫자 증감 시 확대/축소
+  useEffect(() => {
+    setAnimate(true);
+    const timer = setTimeout(() => setAnimate(false), 200);
+    return () => clearTimeout(timer);
+  }, [count]);
+
   return (
     <div className="container">
-      <h1>count:{count}</h1>
-      <CounterBox />
+      <div className={`count-value ${animate ? "changed" : ""}`}>
+        <h1>count:{count}</h1>
+        <CounterBox />
+      </div>
       <div className="count-btn">
         <button onClick={increase}>increase</button>
         <button onClick={decrease}>decrease</button>
